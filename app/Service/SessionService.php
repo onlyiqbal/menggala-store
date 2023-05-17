c9de05b49d1e7092f4d90b59529f24dc9cc13405
iqbal <iqbalmenggala29@gmail.com>
2023-05-17 08:31:26 +0700

    edit file session service

--- app/Service/SessionService.php
+++ app/Service/SessionService.php
@@ -2,6 +2,18 @@
 
 namespace Iqbal\MenggalaStore\Service;
 
+use Iqbal\MenggalaStore\Repository\SessionRepository;
+
 class SessionService
 {
+  public SessionRepository $sessionRepository;
+  public static string $SECRET_KEY = "la7436gferygu4t82736dhg842r8h83r";
+  public static string $COOKIE_NAME = "X-USER-SESSION";
+
+  public function __construct(SessionRepository $sessionRepository)
+  {
+    $this->sessionRepository = $sessionRepository;
+  }
+
+  
 }
